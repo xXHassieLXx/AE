f60b4a36af27d13354d802d22319a4b300f84aac
Hassiel Monjaraz <HassielAM_012@outlook.com>
2024-08-19 09:35:33 -0600

    popop

---
+++ Practica2/js/index.js
@@ -0,0 +1,7 @@
+var miNumero = 10;
+miNumero = 10 + 10;
+miNumero = 2.5;
+console.log(miNumero);
+var booleano = true;
+booleano = false;
+// booleano = 3;  (Esto no es correcto)
---
+++ Practica2/src/index.ts
@@ -0,0 +1,9 @@
+let miNumero: number = 10; 
+
+miNumero = 10 + 10; 
+miNumero = 2.5;
+console.log(miNumero);
+
+let booleano: boolean = true;
+booleano = false; 
+// booleano = 3;  (Esto no es correcto)
